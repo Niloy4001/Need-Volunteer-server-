f3c83ebe87c67f2c9e40e8576b0cc418fb7153f3
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-12-21 22:27:13 +0600

    a get request added for getting post for home page

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+const cors = require("cors")
 const port = 4000
 
-
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.hcojg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
@@ -21,6 +23,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const posts = client.db("NeedVolunteer").collection("posts")
+
+
+    // get posts data for home page
+    app.get('/needVolunteerPost', async(req,res)=>{
+        const result = await posts.find().toArray()
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
