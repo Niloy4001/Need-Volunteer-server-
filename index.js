3dbf4e1f53858b9a34f2c7ca3c21cb81d7a85788
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-12-22 14:44:37 +0600

    get api added for get each post by id

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const cors = require("cors")
-const port = 4000
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = 4000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.hcojg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,32 +24,40 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-    const posts = client.db("NeedVolunteer").collection("posts")
-
+    const posts = client.db("NeedVolunteer").collection("posts");
 
     // get posts data for home page
-    app.get('/needVolunteerPost', async(req,res)=>{
-        const query = {}
-        const option = {
-            sort: { deadline: 1}
-        }
-        const result = await posts.find(query,option).limit(6).toArray()
-        res.send(result)
-    })
+    app.get("/needVolunteerPost", async (req, res) => {
+      const query = {};
+      const option = {
+        sort: { deadline: 1 },
+      };
+      const result = await posts.find(query, option).limit(6).toArray();
+      res.send(result);
+    });
 
-    // post a data form add volunteer post page 
-    app.post('/addPost', async(req,res)=>{
+    // get a single post by id
+    app.get(`/post/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await posts.findOne(query);
+      res.send(result);
+    });
+
+    // post a data form add volunteer post page
+    app.post("/addPost", async (req, res) => {
       const post = req.body;
-      const result = await posts.insertOne(post)
-      res.send(result)
-      // console.log(post);
-      
-    })
+      const result = await posts.insertOne(post);
+      res.send(result);
+      // console.log(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -57,12 +65,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
